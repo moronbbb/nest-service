6d3dae216e9822479db539474c014cf7ac64ec4e
xuan.yang01 <xuan.yang01@liulishuo.com>
2025-02-26 03:52:31 +0800

    callbacks

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { firstValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly appService: AppService,
+  ) { }
+
+  async alert(content: string) {
+    try {
+      await firstValueFrom(
+        this.httpService.post(process.env.WX_BOT_URL!, {
+          msgtype: 'text',
+          text: {
+            // mentioned_list: [],
+            content: `${content}`,
+          },
+        }),
+      );
+    } catch (error) {
+      console.error(`[AlertService]${error?.toString()}`, error);
+      // this.logger.error(error);
+    }
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/events/auth')
+  getEventsAuth(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/events/callback')
+  getEventsCallback(): string {
+    return this.appService.getHello();
+  }
+
+
+  @Post('/events/auth')
+  recieveEventsAuth(@Body() reqBody: any): string {
+    this.alert(JSON.stringify(reqBody ?? ''))
+    return this.appService.getHello();
+  }
+
+  @Post('/events/callback')
+  recieveEventsCallback(@Body() reqBody: any): string {
+    this.alert(JSON.stringify(reqBody ?? ''))
+    return this.appService.getHello();
+  }
+
 }
